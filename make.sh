3bca328e915064297fe4775e05378ee568b09117
Florent Kaisser <florent.pro@kaisser.name>
2017-04-16 13:03:33 +0200

    ajout de la partie "mise en place progressive"

--- make.sh
+++ make.sh
@@ -1,2 +1,3 @@
-asciidoc -a icons -a numbered -a disable-javascript  RDB-CVA.txt
-lowriter --invisible --convert-to odt RDB-CVA.html 
+#asciidoc -a icons -a numbered -a disable-javascript  RDB-CVA.txt
+#lowriter --invisible --convert-to odt RDB-CVA.html 
+asciidoc RDB-CVA.txt
