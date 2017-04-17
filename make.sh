554a19a7dc2fb444b39b871bc765847c76b58383
Florent Kaisser <contact@lille.bike>
2017-04-17 22:45:45 +0200

    modification de la mise en paget et ajout des id pour les parties

--- make.sh
+++ make.sh
@@ -1,3 +1,6 @@
 #asciidoc -a icons -a numbered -a disable-javascript  RDB-CVA.txt
 #lowriter --invisible --convert-to odt RDB-CVA.html 
 asciidoc RDB-CVA.txt
+asciidoc -b "docbook45" RDB-CVA.txt
+dblatex RDB-CVA.xml
+pdftk RDB-CVA.pdf cat 1 5-end output Un\ revenu\ de\ base\ par\ la\ cotisation\ sur\ la\ valeur\ ajoutée\ -\ Florent\ Kaisser\ v2-2.pdf
